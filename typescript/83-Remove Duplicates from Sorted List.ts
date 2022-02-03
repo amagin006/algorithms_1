407a28e6161e4a636fa46322cdaa74e8d966232f
Shota Iwamoto <amaginqoo@gmail.com>
2022-02-02 18:06:03 -0800

    83

---
+++ typescript/83-Remove Duplicates from Sorted List.ts
@@ -0,0 +1,22 @@
+// Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  let currentNode: ListNode = head;
+
+  while (currentNode) {
+    if (currentNode.next && currentNode.val === currentNode.next.val) {
+      currentNode.next = currentNode.next.next;
+    } else {
+      currentNode = currentNode.next;
+    }
+  }
+  return head;
+}
